bba03b891690e3cbb9fa4fc538a7ebd9c2d57b81
Thadeu Esteves Jr <tadeuu@gmail.com>
2020-10-27 02:28:25 -0300

    travis libsodium-1.0.18

--- .travis-libsodium.sh
+++ .travis-libsodium.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz
-tar -xzvf libsodium-1.0.13.tar.gz
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
+tar -xzvf libsodium-1.0.18.tar.gz
 cd libsodium-1.0.13
 ./configure --prefix=/usr
 make && make install
