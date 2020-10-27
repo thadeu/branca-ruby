d336f5b85469debff84da7bc34e0aa58d81aa1ee
Thadeu Esteves Jr <tadeuu@gmail.com>
2020-10-27 02:30:07 -0300

    fix travis ci

--- .travis-libsodium.sh
+++ .travis-libsodium.sh
@@ -3,6 +3,6 @@ set -ex
 
 wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
 tar -xzvf libsodium-1.0.18.tar.gz
-cd libsodium-1.0.13
+cd libsodium-1.0.18
 ./configure --prefix=/usr
 make && make install
